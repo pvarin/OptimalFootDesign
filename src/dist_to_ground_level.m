7c7674daecb26703f6b1934c285653969d5cbc8d
Patrick Varin <patrickjvarin@gmail.com>
2016-12-12 17:30:10 -0500

    removing files

--- src/dist_to_ground_level.m
+++
@@ -1,4 +0,0 @@
-function dist=dist_to_ground_level(q,params)
-    [~, X2] = to_cartesian(q, params.L);
-    dist=(X2(2));
-end
--- src/to_cartesian.m
+++
@@ -1,7 +0,0 @@
-function [X1, X2] = to_cartesian(q, L)
-    theta=q(1);
-    phi=q(2);
-
-    X1 = L*[-sin(theta); cos(theta)];
-    X2 = X1 - L*[sin(phi-theta); cos(phi-theta)];
-end
