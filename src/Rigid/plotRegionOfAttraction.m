73a3d1004c8851499212701f3bb5e44050eb5050
Patrick Varin <patrickjvarin@gmail.com>
2016-12-13 16:57:45 -0500

    Adding figure for the other basin of attraction and added some formatting to the generating script

--- src/Rigid/plotRegionOfAttraction.m
+++ src/Rigid/plotRegionOfAttraction.m
@@ -14,24 +14,32 @@ M = 250;
 N = 250;
 NumSteps = zeros(M,N);
 
-% theta = linspace(0,pi/10,N);
-% phi = 0.29671;
-% d_theta = linspace(-1,0,M);
-% d_phi = -0.4;
+% fixed point
+% q =
+%     0.1552
+%     0.3103
+%    -0.5498
+%    -0.0263
 
 
-theta = linspace(0.145,.16,N);
-phi = 0.3103;
-d_theta = -0.5498;
-d_phi = linspace(0.4,-0.4,M);
+% theta = linspace(0.145,.16,N);
+% phi = 0.3103;
+% d_theta = -0.5498;
+% d_phi = linspace(0.4,-0.4,M);
+
+theta = 0.1552;
+phi = linspace(0.225,0.46,N);
+d_theta = linspace(-0.54,-0.58,M);
+d_phi = -0.0263;
+
 
 tic;
 for i=1:N
     i
     toc
     for j=1:M
-%         q0 = [theta(i); phi; d_theta(j); d_phi];
-        q0 = [theta(i); phi; d_theta; d_phi(j)];
+%         q0 = [theta(i); phi; d_theta; d_phi(j)];
+        q0 = [theta; phi(i); d_theta(j); d_phi];
         for k=1:20
             % solve one timestep
             [T,Q, info] = solve_one_step_level_ground(q0,params);
@@ -47,6 +55,17 @@ for i=1:N
     end
 end
 
-pcolor(theta, d_phi, NumSteps)
-xlabel('\theta')
-ylabel('d\phi/dt')
+% pcolor(theta, d_phi, NumSteps); hold on
+% plot(0.1552, -0.0263,'k+','MarkerSize',20,'LineWidth',2)
+% xlabel('\theta','FontSize',20)
+% ylabel('d\phi/dt','FontSize',20)
+
+pcolor(phi, d_theta, NumSteps); hold on
+plot(0.3103,-0.5498,'k+','MarkerSize',20,'LineWidth',2)
+xlabel('\phi','FontSize',20)
+ylabel('d\theta/dt','FontSize',20)
+
+set(gca,'FontSize',16)
+h = colorbar;
+ylabel(h,'# steps to failure','FontSize',20)
+shading flat
